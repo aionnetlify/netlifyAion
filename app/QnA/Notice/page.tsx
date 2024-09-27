352ea7d874d29686dc4e133828fddcb1db3fc841
김대연 <raccoon200@dejay.co.kr>
2024-09-27 16:09:55 +0900

    test

--- app/QnA/Notice/page.tsx
+++ app/QnA/Notice/page.tsx
@@ -18,13 +18,6 @@ const notices = [
 ];
 
 const NoticePage = () => {
-    const [openIndex, setOpenIndex] = useState<number | null>(null);
-
-    // 공지사항 클릭 시 해당 내용 열기/닫기
-    const toggleNotice = (index: number) => {
-        setOpenIndex(openIndex === index ? null : index);
-    };
-
     return (
         <div className="p-6 max-w-4xl mx-auto mt-[80px]">
             <h1 className="text-3xl font-bold mb-8 text-center">공지사항</h1> {/* 텍스트 중앙 정렬 */}
